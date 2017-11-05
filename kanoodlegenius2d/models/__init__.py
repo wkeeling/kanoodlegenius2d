9299c09a309f846e1bd7e2f24956aa8e6052fe01
Will Keeling <will@zifferent.com>
2017-11-05 21:32:47 +0000

    Add imports

--- kanoodlegenius2d/models/__init__.py
+++ kanoodlegenius2d/models/__init__.py
@@ -1,2 +1,10 @@
 from .base import (initialise,
                    shutdown)
+from .board import Board
+from .game import Game
+from .level import Level
+from .noodle import Noodle
+from .player import Player
+from .playernoodle import PlayerNoodle
+from .puzzle import Puzzle
+from .puzzlenoodle import PuzzleNoodle
