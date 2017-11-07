40fa4bff2448a35c6dbcea1ecc2652ee17e65811
Will Keeling <will@zifferent.com>
2017-11-07 21:08:29 +0000

    More Noodle attributes

--- kanoodlegenius2d/models/noodle.py
+++ kanoodlegenius2d/models/noodle.py
@@ -1,5 +1,17 @@
+from peewee import (CharField,
+                    FixedCharField)
+
 from .base import BaseModel
 
 
 class Noodle(BaseModel):
     """Represents a noodle - a puzzle piece."""
+    name = CharField()
+
+    # The orientations of each part (excluding the root), relative to one another
+    part1 = FixedCharField(max_length=2)
+    part2 = FixedCharField(max_length=2)
+    part3 = FixedCharField(max_length=2)
+    part4 = FixedCharField(max_length=2)
+    part5 = FixedCharField(max_length=2)
+    part6 = FixedCharField(max_length=2)
