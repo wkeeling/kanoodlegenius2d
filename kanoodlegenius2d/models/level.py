5ffaeeff2d4a00f8b20d812076e5e7e06a181b3a
Will Keeling <will@zifferent.com>
2017-11-07 21:16:12 +0000

    Add further fields, __str__

--- kanoodlegenius2d/models/level.py
+++ kanoodlegenius2d/models/level.py
@@ -1,4 +1,6 @@
-from peewee import ForeignKeyField
+from peewee import (CharField,
+                    ForeignKeyField,
+                    IntegerField)
 
 from .base import BaseModel
 from .game import Game
@@ -7,3 +9,8 @@ from .game import Game
 class Level(BaseModel):
     """Represents a level within the game."""
     game = ForeignKeyField(Game, related_name='levels')
+    number = IntegerField()
+    name = CharField(max_length=20)
+
+    def __str__(self):
+        return '<Level: {} ({})>'.format(self.number, self.name)
--- kanoodlegenius2d/models/player.py
+++ kanoodlegenius2d/models/player.py
@@ -9,3 +9,6 @@ class Player(BaseModel):
     """Represents a player playing the game."""
     game = ForeignKeyField(Game)
     name = CharField(max_length=10)
+
+    def __str__(self):
+        return '<Player: {}>'.format(self.name)
