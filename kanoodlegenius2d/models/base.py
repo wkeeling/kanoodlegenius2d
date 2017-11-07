877e1bb2fb6d5557a8709d7e0a3b979aea438f25
Will Keeling <will@zifferent.com>
2017-11-07 21:08:51 +0000

    Start setting up initial data

--- kanoodlegenius2d/models/base.py
+++ kanoodlegenius2d/models/base.py
@@ -5,6 +5,7 @@ from peewee import (Model,
                     SqliteDatabase)
 
 import kanoodlegenius2d.models
+from .noodle import Noodle
 
 _LOG = logging.getLogger(__name__)
 
@@ -12,15 +13,27 @@ database = SqliteDatabase(os.path.join(os.path.expanduser('~'), '.kanoodlegenius
 
 
 def initialise():
-    """Initialise the database, creating the database tables if necessary
-    and establish a connection.
+    """Initialise the database, creating the database tables if they don't already
+    exist, and set up initial data.
     """
     global database
     database.connect()
+
+    # Create the tables if they do not already exist
     for k, v in vars(kanoodlegenius2d.models).items():
         if isinstance(v, type) and issubclass(v, BaseModel):
             _LOG.debug('Creating table {}'.format(k))
-            v.create_table(fail_silently=True)  # Don't error if the tables already exist
+            getattr(v, 'create_table')(fail_silently=True)  # Don't error if the tables already exist
+
+    # Set up the initial data where is does not already exist
+    light_green = Noodle.create(name='light_green')
+    dark_green = Noodle.create(name='dark_green')
+    light_blue = Noodle.create(name='light_blue')
+    dark_blue = Noodle.create(name='dark_blue')
+    yellow = Noodle.create(name='yellow')
+    red = Noodle.create(name='red')
+    pink = Noodle.create(name='pink')
+
 
 
 def shutdown():
