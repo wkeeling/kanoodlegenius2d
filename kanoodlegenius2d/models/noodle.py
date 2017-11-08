3fba2bd524df92621e00a9b9362bc5984c19664f
wkeeling <will.keeling@tororosso.com>
2017-11-08 14:20:29 +0000

    Modify fields, add rotate() method

--- kanoodlegenius2d/models/noodle.py
+++ kanoodlegenius2d/models/noodle.py
@@ -6,10 +6,19 @@ from .base import BaseModel
 
 class Noodle(BaseModel):
     """Represents a noodle - a puzzle piece."""
-    name = CharField()
+    designation = FixedCharField(max_length=1)
+    code = CharField()
 
-    # The orientations of each part (excluding the root), relative to one another
+    # The default orientations of each part (excluding the root), relative to one another
     part1 = FixedCharField(max_length=2)
     part2 = FixedCharField(max_length=2)
     part3 = FixedCharField(max_length=2)
     part4 = FixedCharField(max_length=2)
+
+    def rotate(self, increment=1):
+        """Rotate the noodle clockwise by the specified number of increments.
+
+        Args:
+            increment:
+                The number of increments to rotate the noodle by.
+        """
