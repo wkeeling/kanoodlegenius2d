4ba9c58a2f9b4482fd8073a7d0d5b60f1642049c
wkeeling <will.keeling@tororosso.com>
2017-11-08 14:18:14 +0000

    Level has no direct relationship to Game

--- kanoodlegenius2d/models/level.py
+++ kanoodlegenius2d/models/level.py
@@ -1,14 +1,11 @@
 from peewee import (CharField,
-                    ForeignKeyField,
                     IntegerField)
 
 from .base import BaseModel
-from .game import Game
 
 
 class Level(BaseModel):
     """Represents a level within the game."""
-    game = ForeignKeyField(Game, related_name='levels')
     number = IntegerField()
     name = CharField(max_length=20)
 
