674e6e844e46eca1e9a85a425a6e820c98aac4b6
wkeeling <will.keeling@tororosso.com>
2017-11-08 14:17:36 +0000

    Add place() method

--- kanoodlegenius2d/models/puzzle.py
+++ kanoodlegenius2d/models/puzzle.py
@@ -1,7 +1,9 @@
-from peewee import ForeignKeyField
+from peewee import (ForeignKeyField,
+                    IntegerField)
 
 from .base import BaseModel
 from .level import Level
+from .puzzlenoodle import PuzzleNoodle
 
 
 class Puzzle(BaseModel):
@@ -9,3 +11,16 @@ class Puzzle(BaseModel):
     board preconfigured with some noodles.
     """
     level = ForeignKeyField(Level, 'puzzles')
+    number = IntegerField()
+
+    def place(self, noodle, position):
+        """Place a noodle onto the puzzle in the specified position.
+
+        Args:
+            noodle:
+                The noodle instance to place on the puzzle.
+            position:
+                The position of the root part of the noodle on the puzzle.
+        """
+        PuzzleNoodle.create(puzzle=self, noodle=noodle, position=position, part1=noodle.part1,
+                            part2=noodle.part2, part3=noodle.part3, part4=noodle.part4)
