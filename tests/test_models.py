c79cd4bdd830ebecf21a37b6fe95423e5a82eaa3
wkeeling <will.keeling@tororosso.com>
2017-11-10 15:15:04 +0000

    Add PuzzleTest

--- tests/test_models.py
+++ tests/test_models.py
@@ -7,7 +7,10 @@ from playhouse.test_utils import test_database
 
 from kanoodlegenius2d.models import (Game,
                                      initialise,
+                                     Level,
                                      Noodle,
+                                     Puzzle,
+                                     PuzzleNoodle,
                                      shutdown)
 from kanoodlegenius2d.orientation import Orientation
 
@@ -85,3 +88,36 @@ class NoodleTest(TestCase):
             self.assertEqual(light_blue.part2, Orientation.W)
             self.assertEqual(light_blue.part3, Orientation.SW)
             self.assertEqual(light_blue.part4, Orientation.NW)
+
+
+class PuzzleTest(TestCase):
+
+    def test_place_noodle(self):
+        with test_database(test_db, (Level, Puzzle, PuzzleNoodle, Noodle), create_tables=True):
+            light_blue = Noodle.create(designation='D', code='light_blue',
+                                       part1=Orientation.E,
+                                       part2=Orientation.E,
+                                       part3=Orientation.NE,
+                                       part4=Orientation.SE)
+            level = Level.create(number=1, name='test level')
+            puzzle = Puzzle.create(level=level, number=1)
+
+            puzzle.place(light_blue, 5)
+
+            puzzle_noodle = PuzzleNoodle.get(PuzzleNoodle.position == 5)
+            self.assertEqual(puzzle_noodle.part1, light_blue.part1)
+            self.assertEqual(puzzle_noodle.part2, light_blue.part2)
+            self.assertEqual(puzzle_noodle.part3, light_blue.part3)
+            self.assertEqual(puzzle_noodle.part4, light_blue.part4)
+
+    def test_place_raises_exception_when_root_position_occupied(self):
+        self.fail('Implement')
+
+    def test_place_raises_exception_when_child_position_occupied(self):
+        self.fail('Implement')
+
+    def test_place_raises_exception_when_root_position_off_board(self):
+        self.fail('Implement')
+
+    def test_place_raises_exception_when_child_position_off_board(self):
+        self.fail('Implement')
