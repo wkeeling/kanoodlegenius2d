78f6d748886aebd21e5278569aabd3d45152367a
wkeeling <will.keeling@tororosso.com>
2017-11-16 13:56:52 +0000

    Begin UI

---
+++ kanoodlegenius2d/ui/puzzlescreen.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+
+
+class PuzzleScreen(tk.Frame):
+    """Represents the main screen of the game where a player interacts with
+    a puzzle board and selects noodles to place.
+    """
+    def __init__(self, master=None, cnf=None, **kw):
+        if cnf is None:
+            cnf = {}
+        super().__init__(master, cnf, **kw)
+
+
+class BoardFrame(tk.Frame):
+    """The frame of the PuzzleScreen that contains the board."""
+    def __init__(self, master=None, cnf=None, **kw):
+        if cnf is None:
+            cnf = {}
+        super().__init__(master, cnf, width=480, height=480, bg='black', relief='sunken', **kw)
+
+        # self._board = tk.Canvas
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.geometry('800x480')  # Will eventually be set by the main kanoodlegenius2d root screen
+    puzzle_screen = PuzzleScreen(root)
+    puzzle_screen.pack()
+    board = BoardFrame(puzzle_screen)
+    board.pack()
+    root.mainloop()
