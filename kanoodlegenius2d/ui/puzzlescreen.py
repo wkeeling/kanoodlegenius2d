f332247a7df78d54681d789b5dd9a0c78ff140a9
wkeeling <will.keeling@tororosso.com>
2017-11-17 13:40:14 +0000

    Get basic panels in place

--- kanoodlegenius2d/ui/puzzlescreen.py
+++ kanoodlegenius2d/ui/puzzlescreen.py
@@ -10,15 +10,40 @@ class PuzzleScreen(tk.Frame):
             cnf = {}
         super().__init__(master, cnf, **kw)
 
+        board_and_noodle = tk.Frame(self)
+        board_and_noodle.pack(side='top', fill='x')
+        board_frame = BoardFrame(board_and_noodle, width=500, height=440, bg='black')
+        noodle_frame = NoodleFrame(board_and_noodle, width=300, height=440, bg='gray')
+        board_frame.pack(side='left')
+        noodle_frame.pack(side='left')
+        status_frame = StatusFrame(self, width=800, height=40, bg='black')
+        status_frame.pack()
+
 
 class BoardFrame(tk.Frame):
     """The frame of the PuzzleScreen that contains the board."""
     def __init__(self, master=None, cnf=None, **kw):
         if cnf is None:
             cnf = {}
-        super().__init__(master, cnf, width=480, height=480, bg='black', **kw)
+        super().__init__(master, cnf, **kw)
 
         # self._board = tk.Canvas
+
+
+class NoodleFrame(tk.Frame):
+    """The frame of the PuzzleScreen that contains the noodle."""
+    def __init__(self, master=None, cnf=None, **kw):
+        if cnf is None:
+            cnf = {}
+        super().__init__(master, cnf, **kw)
+
+
+class StatusFrame(tk.Frame):
+    """The bar at the bottom that holds information about the player, current level etc."""
+    def __init__(self, master=None, cnf=None, **kw):
+        if cnf is None:
+            cnf = {}
+        super().__init__(master, cnf, **kw)
 
 
 if __name__ == '__main__':
@@ -26,6 +51,4 @@ if __name__ == '__main__':
     root.geometry('800x480')  # Will eventually be set by the main kanoodlegenius2d root screen
     puzzle_screen = PuzzleScreen(root)
     puzzle_screen.pack(fill='x')
-    board = BoardFrame(puzzle_screen)
-    board.pack(side='left')
     root.mainloop()
