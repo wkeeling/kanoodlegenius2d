249ec88448d28960550b98526e59e59d6d4e51a9
wkeeling <will.keeling@tororosso.com>
2017-11-24 16:50:25 +0000

    Create CanvasWidgetCreator

--- kanoodlegenius2d/ui/component.py
+++ kanoodlegenius2d/ui/component.py
@@ -1,13 +1,37 @@
 
+class CanvasWidgetCreator:
+    """Helper class for rendering widgets on an existing canvas."""
 
-class CanvasButton:
-    """Represents a clickable button drawn onto a canvas widget."""
+    def __init__(self, canvas):
+        """Initialise a new CanvasWidgetCreator with an existing canvas object.
 
-    def __init__(self, canvas, x, y, w, h, text, on_click=None, **kwargs):
-        x1, y1, x2, y2 = x, y, x + w, y + h
-        button = canvas.create_rectangle((x1, y1, x2, y2), outline='white', fill='black')
-        x_offset = (x2 - x1) // 2
-        y_offset = (y2 - y1) // 2
-        text = canvas.create_text(x + x_offset, y + y_offset, text=text,
-                                  fill=kwargs.get('text_colour', 'white'))
+        Args:
+            canvas:
+                The canvas object.
+        """
+        self._canvas = canvas
+
+    def create_button(self, text, centre, onclick, **kwargs):
+        """Create a clickable canvas button.
+
+        Args:
+            text:
+                The text of the button.
+            centre:
+                The x,y point (2-tuple) that the button will be centred around.
+            onclick:
+                The callback for when the button is clicked.
+            kwargs:
+                Additional arguments that can be used to configure the
+                button.
+        """
+        text = self._canvas.create_text(centre[0], centre[1], text=text, fill=kwargs.get('text_colour', 'white'))
+        bbox = self._canvas.bbox(text)
+        padding = 10
+        button = self._canvas.create_rectangle((bbox[0] - padding, bbox[1] - padding,
+                                                bbox[2] + padding, bbox[3] + padding),
+                                               outline='white', fill='black')
+        self._canvas.tag_raise(text)
+        if onclick is not None:
+            self._canvas.tag_bind(button, '<ButtonPress-1>', onclick)
 
