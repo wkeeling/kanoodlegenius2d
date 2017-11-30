6d7e9478ec0f6e8eeb7679bedadc74bee0452b28
wkeeling <will.keeling@tororosso.com>
2017-11-30 13:24:34 +0000

    Calculations for dialog dimensions and positioning

--- kanoodlegenius2d/ui/dialog.py
+++ kanoodlegenius2d/ui/dialog.py
@@ -7,10 +7,10 @@ class Dialog(tk.Toplevel):
     """A popup panel which overlays the frame beneath it and can be closed by the user."""
 
     def __init__(self, message, master=None, cnf=None, **kw):
+        # need timeout that will override buttons
         if cnf is None:
             cnf = {}
         super().__init__(master, cnf, bg='#000000', highlightthickness=1, **kw)
-        print(master.winfo_x(), master.winfo_rootx(), master.winfo_geometry())
 
         # Make the dialog appear on top of its parent
         self.transient(master)
@@ -19,11 +19,17 @@ class Dialog(tk.Toplevel):
         # Remove window border
         self.overrideredirect(True)
 
+        self.update_idletasks()
+        width, height = master.winfo_width() // 2, master.winfo_height() // 2
+        self.geometry("%dx%d+%d+%d" % (width, height,
+                                       master.winfo_rootx() + ((master.winfo_width() - width) // 2),
+                                       master.winfo_rooty() + ((master.winfo_height() - height) // 2)))
 
-        width, height = 500, 300
+        print(master.winfo_x(), master.winfo_y(), master.winfo_geometry())
+        print(master.winfo_rootx(), master.winfo_rooty(), master.winfo_geometry())
 
 
-        self.geometry("%dx%d+%d+%d" % (500, 300, 100, 100))
+
 
         # self.geometry("%dx%d" % (200, 120))
         canvas = tk.Canvas(self, width=500, height=300, bg='#000000', highlightthickness=0)
