2ce2ebd376b15c4347105d38a8d1edb2c92bf3ed
wkeeling <will.keeling@tororosso.com>
2017-11-30 13:37:34 +0000

    Dialog __doc__

--- kanoodlegenius2d/ui/dialog.py
+++ kanoodlegenius2d/ui/dialog.py
@@ -6,11 +6,37 @@ from kanoodlegenius2d.ui.util import CanvasWidgetHelper
 class Dialog(tk.Toplevel):
     """A popup panel which overlays the frame beneath it and can be closed by the user."""
 
-    def __init__(self, message, master=None, cnf=None, **kw):
-        # need timeout that will override buttons
-        if cnf is None:
-            cnf = {}
-        super().__init__(master, cnf, bg='#000000', highlightthickness=1, **kw)
+    def __init__(self, message, master=None, **kwargs):
+        """Initialise and display a new dialog popup.
+
+        Args:
+            message:
+                The text to display on the dialog.
+            master:
+                The parent widget.
+            kwargs:
+                Optional keyword arguments that can include:
+                    width:
+                        The width of the dialog in pixels (default 50% of parent).
+                    height:
+                        The height of the dialog in pixels (default 50% of parent).
+                    show_submit:
+                        Whether to show a submit button (default True).
+                    submit_text:
+                        The text of the submit button (default OK).
+                    on_submit:
+                        Callable invoked when the submit button pressed (default None).
+                    show_cancel:
+                        Whether to show a cancel button (default False).
+                    cancel_text:
+                        The text of the cancel button when shown (default Cancel).
+                    on_cancel:
+                        Callable invoked when the cancel button pressed (default None).
+                    timeout:
+                        The number of seconds after which to automatically submit the
+                        dialog (default None - no auto-submit).
+        """
+        super().__init__(master, bg='#000000', highlightthickness=1)
 
         # Make the dialog appear on top of its parent
         self.transient(master)
@@ -18,21 +44,15 @@ class Dialog(tk.Toplevel):
         self.grab_set()
         # Remove window border
         self.overrideredirect(True)
-
+        # Make the geometry update
         self.update_idletasks()
+        
         width, height = master.winfo_width() // 2, master.winfo_height() // 2
         self.geometry("%dx%d+%d+%d" % (width, height,
                                        master.winfo_rootx() + ((master.winfo_width() - width) // 2),
                                        master.winfo_rooty() + ((master.winfo_height() - height) // 2)))
 
-        print(master.winfo_x(), master.winfo_y(), master.winfo_geometry())
-        print(master.winfo_rootx(), master.winfo_rooty(), master.winfo_geometry())
-
-
-
-
-        # self.geometry("%dx%d" % (200, 120))
-        canvas = tk.Canvas(self, width=500, height=300, bg='#000000', highlightthickness=0)
+        canvas = tk.Canvas(self, width=width, height=height, bg='#000000', highlightthickness=0)
         canvas.pack()
         widget_helper = CanvasWidgetHelper(canvas)
         widget_helper.create_button(' OK ', (100, 100), font='helvetica', onclick=lambda: self.destroy())
