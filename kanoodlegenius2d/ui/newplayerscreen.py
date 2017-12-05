496bcd70932accbaaa0845d655eb0bf3073b70d1
wkeeling <will.keeling@tororosso.com>
2017-12-05 08:58:03 +0000

    Refactor

--- kanoodlegenius2d/ui/newplayerscreen.py
+++ kanoodlegenius2d/ui/newplayerscreen.py
@@ -22,11 +22,11 @@ class NewPlayerScreen(tk.Frame):
         self._canvas.create_text(400, 60, text='Enter Player Name', font=('helvetica', 22),
                                  justify='center', fill='#FFFFFF')
 
-        self._player_text = self._canvas.create_text(400, 120, text='', font=('helvetica', 18),
+        self._player_name = self._canvas.create_text(400, 120, text='', font=('helvetica', 18),
                                                      justify='center', fill='#FFFFFF')
         self._canvas.create_line(250, 135, 550, 135, fill='#666666', width=2.0)
 
-        self._keys = self._init_keyboard()
+        self._buttons = self._init_keyboard()
 
     def _init_keyboard(self):
         rows = (
@@ -36,42 +36,42 @@ class NewPlayerScreen(tk.Frame):
             ('u', 'v', 'w', 'x', 'y', 'z')
         )
 
-        args = {
+        button_args = {
             'font': ('helvetica', 16),
             'width': 50,
             'height': 50
         }
 
-        keys = []
+        buttons = []
         y, offset = 220, 55
         for row in rows:
             x = 150
             for char in row:
-                keys.append(CanvasButton(self._canvas, str(char), (x, y), onclick=self._onclick, **args))
+                buttons.append(CanvasButton(self._canvas, str(char), (x, y), onclick=self._onkeypress, **button_args))
                 x += offset
             y += offset
 
-        args['font'] = ('helvetica', 14)
-        CanvasButton(self._canvas, 'Del', (x, y - offset), onclick=self._ondelete, **args)
+        button_args['font'] = ('helvetica', 14)
+        CanvasButton(self._canvas, 'Del', (x, y - offset), onclick=self._ondelete, **button_args)
         x += offset
-        CanvasButton(self._canvas, 'Shift', (x, y - offset), onclick=self._onshift, lockable=True, **args)
+        CanvasButton(self._canvas, 'Shift', (x, y - offset), onclick=self._onshift, lockable=True, **button_args)
         x += offset
-        CanvasButton(self._canvas, 'Exit', (x, y - offset), onclick=self._oncancel, **args)
+        CanvasButton(self._canvas, 'Exit', (x, y - offset), onclick=self._oncancel, **button_args)
         x += offset
-        CanvasButton(self._canvas, 'Done', (x, y - offset), onclick=self._onsubmit, **args)
+        CanvasButton(self._canvas, 'Done', (x, y - offset), onclick=self._onsubmit, **button_args)
 
-        return keys
+        return buttons
 
-    def _onclick(self, key):
-        self._canvas.itemconfigure(self._player_text,
-                                   text=self._canvas.itemcget(self._player_text, 'text') + str(key))
+    def _onkeypress(self, key):
+        self._canvas.itemconfigure(self._player_name,
+                                   text=self._canvas.itemcget(self._player_name, 'text') + str(key))
 
     def _ondelete(self, _):
-        self._canvas.itemconfigure(self._player_text,
-                                   text=self._canvas.itemcget(self._player_text, 'text')[:-1])
+        self._canvas.itemconfigure(self._player_name,
+                                   text=self._canvas.itemcget(self._player_name, 'text')[:-1])
 
     def _onshift(self, _):
-        for key in self._keys:
+        for key in self._buttons:
             if key.text.isalpha():
                 if key.text.islower():
                     key.text = key.text.upper()
