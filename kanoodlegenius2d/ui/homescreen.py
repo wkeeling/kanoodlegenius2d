ef808c081f1f98f95cb1b5a80c003ad105bf8d9b
Will Keeling <will@zifferent.com>
2017-12-08 22:38:44 +0000

    Make title text bigger, remove main function

--- kanoodlegenius2d/ui/homescreen.py
+++ kanoodlegenius2d/ui/homescreen.py
@@ -20,7 +20,7 @@ class HomeScreen(tk.Frame):
         canvas = tk.Canvas(self, width=800, height=480, bg='#000000', highlightthickness=0)
         canvas.pack()
 
-        canvas.create_text(400, 60, text='Kanoodle Genius 2D', font=('helvetica', 22),
+        canvas.create_text(400, 100, text='Kanoodle Genius 2D', font=('helvetica', 32),
                            justify='center', fill='#FFFFFF')
 
         args = {
@@ -29,15 +29,5 @@ class HomeScreen(tk.Frame):
             'font': 'helvetica'
         }
 
-        CanvasButton(canvas, 'NEW PLAYER', (305, 200), onclick=lambda _: onnewplayer(), **args)
-        CanvasButton(canvas, 'EXISTING PLAYER', (485, 200), onclick=lambda _: onexistingplayer(), **args)
-
-if __name__ == '__main__':
-    root = tk.Tk()
-    root.geometry('800x480+500+300')  # Will eventually be set by the main kanoodlegenius2d root screen
-    game_screen = HomeScreen(lambda: None, lambda: None, master=root, highlightthickness=2)
-    game_screen.pack(fill='x')
-    root.attributes('-topmost', True)
-    root.update()
-    root.attributes('-topmost', False)
-    root.mainloop()
+        CanvasButton(canvas, 'NEW PLAYER', (305, 220), onclick=lambda _: onnewplayer(), **args)
+        CanvasButton(canvas, 'EXISTING PLAYER', (485, 220), onclick=lambda _: onexistingplayer(), **args)
