e2a24120fcd71cece986cc05c38d6b7969acfaac
Will Keeling <will@zifferent.com>
2017-12-17 09:25:03 +0000

    Add Mac specific font configuration

--- kanoodlegenius2d/ui/settings.py
+++ kanoodlegenius2d/ui/settings.py
@@ -6,7 +6,7 @@ fonts = {
     'homescreen_kanoodle': ('wood stamp', 80),
     'homescreen_genius': ('KG Counting Stars', 60),
     'homescreen_2d': ('cube vol.2', 36),
-    'screen_title': ('wood stamp', 48),
+    'screen_title': ('wood stamp', 36),
     'gamescreen_player': ('wood stamp', 28),
     'gamescreen_status': ('wood stamp', 20),
     'player_name': ('FreeSans', 18),
@@ -17,5 +17,14 @@ fonts = {
 }
 
 # System specific font overrides
-if platform.system() == 'Darwin':
-    fonts['button_standard'] = 'helvetica'
+if platform.system() == 'Darwin':  # MacOS
+    fonts['homescreen_kanoodle'] = ('wood stamp', 104)
+    fonts['homescreen_genius'] = ('KG Counting Stars', 78)
+    fonts['homescreen_2d'] = ('cube vol.2', 48)
+    fonts['screen_title'] = ('wood stamp', 48)
+    fonts['button_standard'] = ('helvetica', 16)
+    fonts['button_keyboard'] = ('helvetica', 18)
+    fonts['player_name'] = ('helvetica', 22)
+    fonts['gamescreen_player'] = ('wood stamp', 38)
+    fonts['gamescreen_status'] = ('wood stamp', 28)
+
