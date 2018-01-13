a41f1d6a61aed4fbdaf87b6b07fa6bb123925b97
Will Keeling <will@zifferent.com>
2018-01-13 11:29:43 +0000

    Fix to display transparent png

--- kanoodlegenius2d/ui/gamescreen.py
+++ kanoodlegenius2d/ui/gamescreen.py
@@ -49,7 +49,31 @@ class GameScreen(tk.Frame):
         if not NOODLE_IMAGES:
             for noodle in Noodle.select():
                 image = Image.open(os.path.join(os.path.dirname(__file__), 'images', noodle.image)).convert('RGBA')
-                NOODLE_IMAGES[noodle.designation] = ImageTk.PhotoImage(image)
+                NOODLE_IMAGES[noodle.designation] = ImageTk.PhotoImage(self._flatten_alpha(image))
+
+    def _flatten_alpha(self, img):
+        """Thanks to https://stackoverflow.com/questions/41576637/are-rgba-pngs-unsupported-in-python-3-5-pillow"""
+        alpha = img.split()[-1]  # Pull off the alpha layer
+        ab = alpha.tobytes()  # Original 8-bit alpha
+
+        checked = []  # Create a new array to store the cleaned up alpha layer bytes
+
+        # Walk through all pixels and set them either to 0 for transparent or 255 for opaque fancy pants
+        transparent = 50  # change to suit your tolerance for what is and is not transparent
+
+        p = 0
+        for pixel in range(0, len(ab)):
+            if ab[pixel] < transparent:
+                checked.append(0)  # Transparent
+            else:
+                checked.append(255)  # Opaque
+            p += 1
+
+        mask = Image.frombytes('L', img.size, bytes(checked))
+
+        img.putalpha(mask)
+
+        return img
 
 
 class BoardFrame(tk.Frame):
